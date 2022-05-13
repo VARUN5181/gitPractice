9c75621f05993ca445562e7bf0be3a6bc91ab944
Varun <muthojuvarun555@gmail.com>
2022-05-13 11:17:22 +0530

    First Commit

--- src/test/java/demo.java
+++ src/test/java/demo.java
@@ -1,22 +1,20 @@
-import org.openqa.selenium.By;
+import org.junit.Assert;
+import org.junit.Test;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
-import java.util.List;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class demo {
-    public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver", "C:\\Chrome Driver\\chromedriver_win32\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-
-        driver.get("https://www.amazon.in/gp/cart/view.html?ref_=nav_cart");
-
-        List<WebElement> links = driver.findElements(By.tagName("a"));
-
-        System.out.println(links);
-
-        driver.quit();
-
+    @Test
+    public void OpenBrowser()  {
+        WebDriver driver;
+        System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("headless");
+        options.addArguments("disable-gpu");
+        driver = new ChromeDriver(options);
+        driver.get("https://www.google.com");
+        System.out.println("Title of the page is: " + driver.getTitle());
+        Assert.assertEquals("Page title is not correct", "Google", driver.getTitle());
     }
 }
